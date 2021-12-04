3d1294f0fd7e38090f4a489df0a5dd975dd604b1
Sid N <7568554+unrealsid@users.noreply.github.com>
2021-12-04 11:16:56 +0530

    Added a galaxy shader

--- vk-game-engine/src/vk_shader_config.h
+++ vk-game-engine/src/vk_shader_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define VERT_SHADER_LOCATION "C:\\Users\\siddh\\Documents\\Visual Studio 2019\\Projects\\vk-simple-game-engine\\vk-game-engine\\shaders\\quad.vert.spv"
-#define FRAG_SHADER_LOCATION "C:\\Users\\siddh\\Documents\\Visual Studio 2019\\Projects\\vk-simple-game-engine\\vk-game-engine\\shaders\\quad.frag.spv"
+#define FRAG_SHADER_LOCATION "C:\\Users\\siddh\\Documents\\Visual Studio 2019\\Projects\\vk-simple-game-engine\\vk-game-engine\\shaders\\galaxy.frag.spv"
 #define TEXTURE_LOCATION "C:\\Users\\siddh\\Documents\\Visual Studio 2019\\Projects\\vk-simple-game-engine\\vk-game-engine\\shaders\\texture.jpg"
 
 //C:\\Users\\siddh\\Documents\\Visual Studio 2019\\Projects\\vk-simple-game-engine\\vk-game-engine\\
